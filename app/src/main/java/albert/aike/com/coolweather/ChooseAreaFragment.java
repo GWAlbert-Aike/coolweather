e4363ac8de1665781506526bd8f0e523f833a951
Albert <2495774179@qq.com>
2018-12-10 22:58:28 +0800

    城市天气预报获取成功

--- app/src/main/java/albert/aike/com/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/albert/aike/com/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package albert.aike.com.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -89,9 +90,15 @@ public class ChooseAreaFragment extends Fragment {
                 if(currentLevel == LEVEL_PROVINCE){
                     selectedProvince = provinceList.get(position);
                     queryCities();
-                }else{
+                }else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounies();
+                }else if(currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
